63a5b53f6e7ae132051a69bb9473d6e04af2412a
Luis <arturo_mendoza@outlook.com>
2022-04-12 11:49:25 -0500

    Añade línea, arregla código con export default

--- weekly_mission_1/example10/pokemon.js
+++ weekly_mission_1/example10/pokemon.js
@@ -1,12 +1,12 @@
 
-export class MyPokemon{
+export default class MyPokemon{
     constructor(name){
         this.name= name
        }
     sayHello(){
         console.log(`${this.name}, says: Hello!!!`)
     }
+    sayMessage(msg){
+        console.log(`Mi pokemon ${this.name} dice: ${msg}`)
+      }
 }
-
-
-sdds
