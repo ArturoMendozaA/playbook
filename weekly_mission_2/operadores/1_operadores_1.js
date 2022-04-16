6243a59d06c3d0aac4980071e1b01f881fb57733
Luis <arturo_mendoza@outlook.com>
2022-04-16 12:02:40 -0500

    Añade ejercicio1

---
+++ weekly_mission_2/operadores/1_operadores_1.js
@@ -0,0 +1,10 @@
+//Ejemplo for Each para imprimir elementos de una lista
+const numbers=[1, 2, 3, 4, 5];
+console.log("Ejemplo !: Imprimiendo los elementos de una lista")
+numbers.forEach(num => console.log(num))
+
+//otra lista
+const otherList= [1, 2, 4, 5, 6]
+
+console.log("Mi otra lista")
+otherList.forEach(eachNum => console.log(eachNum))
